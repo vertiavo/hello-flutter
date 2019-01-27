313d434e793fa8ab5dec456493ccfc66e12d580e
Marek Jakubowski <vertiavo@gmail.com>
2019-01-27 15:34:16 +0100

    Modify app style

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-    child: Text(
-      "Hello Flutter!",
-      textDirection: TextDirection.ltr,
+  runApp(MaterialApp(
+    title: "Hello Flutter App",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Title in App Bar"),
+      ),
+      body: Material(
+        color: Colors.deepPurple,
+        child: Center(
+          child: Text(
+            "Hello Flutter!",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white, fontSize: 36.0),
+          ),
+        ),
+      ),
     ),
   ));
 }
