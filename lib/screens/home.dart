15a9539b26ef7da313b7f01f260f2fe0cd38fdcb
Marek Jakubowski <vertiavo@gmail.com>
2019-01-27 15:50:46 +0100

    Added hello function

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,11 +7,32 @@ class Home extends StatelessWidget {
       color: Colors.deepPurple,
       child: Center(
         child: Text(
-          "Hello Flutter!",
+          sayHello(),
           textDirection: TextDirection.ltr,
           style: TextStyle(color: Colors.white, fontSize: 36.0),
         ),
       ),
     );
+  }
+
+  String sayHello() {
+    String hello;
+
+    DateTime now = new DateTime.now();
+    int hour = now.hour;
+    int minute = now.minute;
+
+    if (hour < 12) {
+      hello = "Good Morning";
+    } else if (hour < 18) {
+      hello = "Good Afternoon";
+    } else {
+      hello = "Good Evening";
+    }
+
+    String minutes =
+        (minute < 10) ? "0" + minute.toString() : minute.toString();
+
+    return "It's now " + hour.toString() + ":" + minutes + ". \n" + hello;
   }
 }
