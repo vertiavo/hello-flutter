9def3320fbaaaac9cb766d8fad66efb345f4bfda
Marek Jakubowski <vertiavo@gmail.com>
2019-01-27 15:37:56 +0100

    Moved login into class

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: "Hello Flutter App",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Title in App Bar"),
-      ),
-      body: Material(
-        color: Colors.deepPurple,
-        child: Center(
-          child: Text(
-            "Hello Flutter!",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.white, fontSize: 36.0),
+void main() => runApp(new HelloFlutterApp());
+
+class HelloFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello Flutter App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Title in App Bar"),
+        ),
+        body: Material(
+          color: Colors.deepPurple,
+          child: Center(
+            child: Text(
+              "Hello Flutter!",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 36.0),
+            ),
           ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
